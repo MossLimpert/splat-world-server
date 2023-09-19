106595556a966879c72e670fd5d4d41de9e6c412
Moss Limpert <70284036+MossLimpert@users.noreply.github.com>
2023-09-19 15:18:25 -0400

    added minio things
    hopefully i dont break the project when i update node.

---
+++ server/objectstorage.js
@@ -0,0 +1,22 @@
+const minio = require('minio');
+
+const minioClient = new minio.Client({
+    endPoint: '127.0.0.1',
+    port: 9000,
+    useSSL: true,
+    accessKey: 'mosslimpert',
+    secretKey: 'Monster-Salad4'
+});
+
+//let file = '../hosted/img/bubbles.png';
+
+const tryGetBuckets = async () => {
+    try {
+        const buckets = await minioClient.listBuckets();
+        console.log('Success ', buckets);
+    } catch (err) {
+        console.log(err.message);
+    }
+};
+
+tryGetBuckets();
