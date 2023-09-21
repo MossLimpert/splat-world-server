79ff097511af634d86eaaf4adfccc596c8e1ea9b
Moss Limpert <70284036+MossLimpert@users.noreply.github.com>
2023-09-21 10:30:49 -0400

    test commit

--- server/objectstorage.js
+++ server/objectstorage.js
@@ -10,13 +10,15 @@ const minioClient = new minio.Client({
 
 //let file = '../hosted/img/bubbles.png';
 
-const tryGetBuckets = async () => {
-    try {
-        const buckets = await minioClient.listBuckets();
-        console.log('Success ', buckets);
-    } catch (err) {
-        console.log(err.message);
-    }
-};
+// const tryGetBuckets = async () => {
+//     try {
+//         const buckets = await minioClient.listBuckets();
+//         console.log('Success ', buckets);
+//     } catch (err) {
+//         console.log(err.message);
+//     }
+// };
 
-tryGetBuckets();
+// tryGetBuckets();
+
+module.exports = minioClient;
