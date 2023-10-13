8e67e151659380a9b108757806c426ea8147a35d
Moss Limpert <70284036+MossLimpert@users.noreply.github.com>
2023-10-13 15:45:25 -0400

    converted test functions into parameterized functions

--- server/objectstorage.js
+++ server/objectstorage.js
@@ -10,15 +10,15 @@ const minioClient = new minio.Client({
 });
 
 // names of buckets:
-const crewHeader = 'crew-header';
-const tagImg = 'tag-img';
-const tag = 'tag';
-const userHeader = 'user-header';
-const userPfp = 'user-pfp';
-console.log(crewHeader,tagImg,tag,userHeader); // so i dont get lint errors
+// const crewHeader = 'crew-header';
+// const tagImg = 'tag-img';
+// const tag = 'tag';
+// const userHeader = 'user-header';
+// const userPfp = 'user-pfp';
+// console.log(crewHeader,tagImg,tag,userHeader); // so i dont get lint errors
 
 // file for testing
-const file = '../hosted/img/bubbles.png';
+//const file = '../hosted/img/bubbles.png';
 
 // get list of buckets
 const getBuckets = async () => {
@@ -30,15 +30,11 @@ const getBuckets = async () => {
   }
 };
 
-// send test image to user pfp from file location
-const testSendFromFilePath = async () => {
+// send object to bucket from file path
+const sendFromFilePath = async (metadata, bucketName, objectName, filePath) => {
   try {
-    const metaData = {
-      test: 'value',
-    };
-
     // send image to server using filepath
-    const result = await minioClient.fPutObject(userPfp, 'test', file, metaData, (err, objInfo) => {
+    const result = await minioClient.fPutObject(bucketName, objectName, filePath, metadata, (err, objInfo) => {
       if (err) {
         console.log(err);
         return { error: err };
@@ -56,60 +52,155 @@ const testSendFromFilePath = async () => {
   }
 };
 
-// send test image to user pfp from buffer
-const testSendFromBuffer = async () => {
-    try {
-        const metaData = {
-            test: 'value',
-        };
-        // open filestream
-        let fileStream = fs.createReadStream(file);
+// send object to bucket from buffer
+const sendFromFileStreamBuffer = async (metadata, bucketName, objectName, filePath) => {
+  try {
+    // open filestream
+    let fileStream = fs.createReadStream(filePath);
 
-        // check make sure file exists, get statistics about it
-        const object = fs.stat(
-            file,
-            async (err, stats) => {
-                if (err) {
-                    console.log(err);
-                    return {error: err};
-                }
-                
-                // print file stats
-                console.log(stats);
+    // check make sure file exists, get statistics about it
+    const object = fs.stat(
+        file,
+        async (err, stats) => {
+            if (err) {
+                console.log(err);
+                return {error: err};
+            }
+            
+            // print file stats
+            console.log(stats);
 
-                // send buffer to server
-                const result = await minioClient.putObject(
-                    userPfp,
-                    'test_from_buffer',
-                    fileStream,
-                    metaData,
-                    (error, objInfo) => {
-                        if (error) {
-                            console.log(error);
-                            return {error: error};
-                        }
+            // send buffer to server
+            const result = await minioClient.putObject(
+                bucketName,
+                objectName,
+                fileStream,
+                metadata,
+                (error, objInfo) => {
+                    if (error) {
+                        console.log(error);
+                        return {error: error};
+                    }
 
-                        console.log('Success!', objInfo.etag, objInfo.versionId);
-                        return objInfo;
-                    });
+                    console.log('Success!', objInfo.etag, objInfo.versionId);
+                    return objInfo;
+                });
 
-                //console.log(result.etag);
-                return result;
-            });
-        console.log(object.etag);
-        return object;
-    } catch (err) {
-        console.log(err);
-        return { error: err };
-    }
+            //console.log(result.etag);
+            return result;
+        });
+    console.log(object.etag);
+    return object;
+} catch (err) {
+    console.log(err);
+    return { error: err };
+}
 };
 
-testSendFromBuffer();
+const sendFromStringBuffer = async (metadata, bucketName, objectName, buffer) => {
+  try {
+    // send buffer to server
+    const result = await minioClient.putObject(
+        bucketName,
+        objectName,
+        buffer,
+        metadata,
+        (error, objInfo) => {
+          if (error) {
+              console.log(error);
+              return {error: error};
+          }
+
+          console.log('Success!', objInfo.etag, objInfo.versionId);
+          return objInfo;
+      });
+
+    //console.log(result.etag);
+    return result;
+} catch (err) {
+    console.log(err);
+    return { error: err };
+}
+};
+
+// send test image to user pfp from file location
+// const testSendFromFilePath = async () => {
+//    try {
+//     const metaData = {
+//       test: 'value',
+//     };
+
+//     // send image to server using filepath
+//     const result = await minioClient.fPutObject(userPfp, 'test', file, metaData, (err, objInfo) => {
+//       if (err) {
+//         console.log(err);
+//         return { error: err };
+//       }
+
+//       console.log('Success!', objInfo.etag, objInfo.versionId);
+//       return objInfo;
+//     });
+
+//     console.log(result.etag);
+//     return result;
+//   } catch (err) {
+//     console.log(err);
+//     return { error: err };
+//   }
+// };
+
+// send test image to user pfp from buffer
+// const testSendFromBuffer = async () => {
+//     try {
+//         const metaData = {
+//             test: 'value',
+//         };
+//         // open filestream
+//         let fileStream = fs.createReadStream(file);
+
+//         // check make sure file exists, get statistics about it
+//         const object = fs.stat(
+//             file,
+//             async (err, stats) => {
+//                 if (err) {
+//                     console.log(err);
+//                     return {error: err};
+//                 }
+                
+//                 // print file stats
+//                 console.log(stats);
+
+//                 // send buffer to server
+//                 const result = await minioClient.putObject(
+//                     userPfp,
+//                     'test_from_buffer',
+//                     fileStream,
+//                     metaData,
+//                     (error, objInfo) => {
+//                         if (error) {
+//                             console.log(error);
+//                             return {error: error};
+//                         }
+
+//                         console.log('Success!', objInfo.etag, objInfo.versionId);
+//                         return objInfo;
+//                     });
+
+//                 //console.log(result.etag);
+//                 return result;
+//             });
+//         console.log(object.etag);
+//         return object;
+//     } catch (err) {
+//         console.log(err);
+//         return { error: err };
+//     }
+// };
 
 module.exports = {
     minioClient,
     getBuckets,
-    testSendFromFilePath,
-    testSendFromBuffer,
-
+    sendFromFilePath,
+    sendFromFileStreamBuffer,
+    sendFromStringBuffer
 };
